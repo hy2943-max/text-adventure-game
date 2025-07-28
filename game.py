4cd986fb5b1c56060af4da906bcb282ab96448c9
hy2943-max <hy2943@columbia.edu>
2025-07-29 00:26:43 +0800

    Update game.py

--- game.py
+++ game.py
@@ -1,57 +1,40 @@
-def location_1(has_key):
-    print("You're in a beautiful field.")
+# Write you adventure here!
+def game_over():
+    print("GAME OVER")
+
+def location_1():
+    print("You're in a forest and you got a tent.")
     print("ACTION MENU:")
-    print("- walk east")
-    print("- walk west")
-    if not has_key:
-        print("- pickup key")
-
+    print("- burn the tent")
+    print("- sleep in it at night")
     action = input("INPUT ACTION: ")
-
-    if action == "walk east":
-        print("You walk east towards a house.")
-        location_2(has_key)
-    elif action == "walk west":
-        print("You walk west. The field continues.")
-        location_4(has_key)
-    elif action == "pickup key" and not has_key:
-        print("You pick up the key.")
-        has_key = True
-        location_1(has_key)
+    if action == "sleep in it at night":
+        print("You wake up fresh and continue to explore.")
+        location_2()
+    elif action == "burn the tent":
+        print("You died at night.")
+        game_over()
     else:
         print(f"Invalid action: {action}")
-        location_1(has_key)
+        location_1()
 
-
-def location_2(has_key):
-    print("You see a house with a locked door.")
+def location_2():
+    print("A leprechaun is attrated by your tent, and he said he can lead you to your friend.")
     print("ACTION MENU:")
-    print("- walk west")
-    if has_key:
-        print("- use key")
-
+    print("- follow him")
+    print("- reject him")
     action = input("INPUT ACTION: ")
-
-    if action == "walk west":
-        location_1(has_key)
-    elif action == "use key" and has_key:
-        location_3(has_key)
+    if action == "follow him":
+        location_3()
+    elif action == "reject him" :
+        print("He is mad and turns you into a frog")
+        game_over()
     else:
         print(f"Invalid action: {action}")
-        location_2(has_key)
+        location_2()
 
+def location_3():
+    print("The leprechaun helped you to find your friend. You won!")
 
-def location_3(has_key):
-    print("You enter the house. You won!")
+location_1()
 
-
-def location_4(has_key):
-    print("You fall into a pit, GAME OVER")
-
-
-def main():
-    has_key = False
-    location_1(has_key)
-
-
-main()
